be63abbad8fb7d68bb42b11ded4709697f9d92f5
Saqib Bilal <saqib_bilal786@yahoo.com>
2024-09-19 14:37:13 -0400

    changed vh to dvh in styles

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+    // if valid{
+        // submit form
+        // hideform
+        // Show success
+
+    // }
+    // else{
+        // validation error
+// }
+})
+
+function switchPage(){
+    
+}
